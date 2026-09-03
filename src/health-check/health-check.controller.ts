3bad2e5fc109705c8f45c020b3f4c6254f5ed379
barfer-dev <barferalimentos@gmail.com>
2026-09-03 11:38:29 +0300

    health check

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/')
+export class HealthCheckController {
+
+    @Get()
+    healthCheck() {
+        return 'client gateway is up and running';
+    }
+
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config/envs';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { ExceptionFilter } from './common/exceptions/rpc-exception.filter';
 
 async function bootstrap() {
   const logger = new Logger('gateway');
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api' ,{
+    exclude: [{ path: '', method: RequestMethod.GET }]
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
